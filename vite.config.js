a2fb27653dad8e802da1c6c1bf837d219f5c1240
juandi022 <104865095+juandi022@users.noreply.github.com>
2026-04-14 21:42:18 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_DEPLOY_BASE_URL ?? '/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  base: '/bodyprueba/',   // ruta fija para tu repo en GitHub Pages
+  plugins: [react()],
 })
